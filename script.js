45f8e727615b88e70b650f19caf9e0da5643765c
jameseversmile <jameseversmile@gmail.com>
2025-05-21 06:50:18 -0700

    for loop tutorials

---
+++ script.js
@@ -0,0 +1,58 @@
+// parts of loop
+// initializer, increment, decrement, and reset
+// e.g for (initializer; condition; increment/decrement) {
+//        code to be executed
+// }
+
+// for (let i = 0; i < 5; i++) {
+//      console.log('this is the value', i);
+// }
+// console.log('this is the value'); 
+
+// for (let startingNumber = 1; startingNumber < 50; startingNumber++) {
+//    console.log('this is the value', startingNumber);
+
+//      if (startingNumber % 2 === 0) {
+//         console.log('this is an even number', startingNumber);
+//    }
+//    else {
+//         console.log('this is an odd number', startingNumber);
+//    }
+// } 
+
+//  for (let i = 5; i <= 20; i++) {
+//    console.log('this is the value', i);
+//  } 
+
+//  let score = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+//      num = 5;
+//      console.log(score.length);
+//      console.log(score[0]);
+//      console.log(score[num]);
+//      for (let i = 0; i < 4; i++) {
+//          console.log('this is the value', score[i]);
+//      }
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log('this is the value', i);
+//     if (i === 90) {
+//         console.log('final value', i);
+//         continue;
+//     }
+// }for (let i = 0; i < 10; i++) {
+
+//      if (i % 2 === 0 && i !== 4) { // check if the number is even and not equal to 4, 4 will not be printed as even
+//          console.log('this is an even number', i);
+//      } else {
+//          console.log('this is an odd number', i);
+//      }
+// }
+ 
+// for (let i = 0; i < 10; i++) {
+// if (i % 2 === 0 && i !== 4) { 
+//          console.log('this is an even number', i);
+//      } else {
+//          console.log('this is an odd number', i);
+//      }
+//  }
+
